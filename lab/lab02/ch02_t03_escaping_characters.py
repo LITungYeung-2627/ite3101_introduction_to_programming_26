105c92c07ff6bc228699cb66396ae604cc5f0ffe
LITungYeung-2627 <litungyeung1711@gmail.com>
2026-09-15 02:21:09 +0000

    [skip actions] lab/lab02/ch02_t03_escaping_characters.py

--- lab/lab02/ch02_t03_escaping_characters.py
+++ lab/lab02/ch02_t03_escaping_characters.py
@@ -1,3 +1,3 @@
 # The string below is broken. Fix it using the escape backslash!
 # Uncomment the following line!
-hello = "This isn\'t flying, this is falling with style!"
+hello = "This isn't flying, this is falling with style!"

7329506d5a26ef16d0c6f1e786b00c0145d1812b
LITungYeung-2627 <litungyeung1711@gmail.com>
2026-09-15 02:09:08 +0000

    lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
 # Assign your variables below, each on its own line!
-caeser = "Graham"
+caesar = "Graham"
 praline = "John"
 viking = "Teresa"
-print(caeser)
+print(caesar)
 print(praline)
 print(viking)
